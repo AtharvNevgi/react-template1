faece18e5dba7455b2a6eb7abf4c64e97fefc3ab
Atharva Nevgi <atharva.nevgi579@gmail.com>
2025-06-02 01:43:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './pages/Navbar.jsx'
+
+function App() {
+
+  return (
+    <>
+        <div className="container">
+            <Navbar />
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,11 @@
+import '../styles/navbar.css'
+
+function Navbar(){
+    return(
+        <div className="navbar">
+            <div className="nav-left"></div>
+            <div className="nav-right"></div>
+        </div>
+    );
+}
+export default Navbar
