a4600412ad1d1374d363a6a3de9836530e86eede
AtharvNevgi <atharva.nevgi579@gmail.com>
2025-06-03 20:28:10 +0530

    added content to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,15 @@ function Home() {
   return (
     <>
       <Navbar />
+      <div className="main">
+        <div className="main-left">
+          <h3>Own Your Daily Routine</h3>
+          <h1 className="heading">0.4 TrackMate XE</h1>
+          <h3>A health tracker that motivates you every step of the way</h3>
+          <button className="buyNow">Buy Now</button>
+        </div>
+        <div className="main-right"></div>
+      </div>
     </>
   );
 }
