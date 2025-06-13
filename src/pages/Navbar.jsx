46d8326b7ded21dbe04f5ddfd9d6d04d5b40e8eb
AtharvNevgi <atharva.nevgi579@gmail.com>
2025-06-13 20:32:54 +0530

    added sideBar with close funtionality

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,25 +1,43 @@
 import '../styles/navbar.css'
 import logo from '../assets/images/logo.jpg';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navbar() {
-    return (
-        <div className="navbar">
-            <div className="nav-left">
+    const [sideBar, setSideBar] = useState(true);
 
-                <a href='/'><img className='logo' src={logo} alt="" /></a>
-                <a href="/">
-                    <h1>TrackMate</h1>
-                </a>
+    const handleSideBar = () => {
+        if (sideBar) {
+            setSideBar(false);
+        }
+        else {
+            setSideBar(true)
+        }
+    }
+    const closeSideBar = () => {
+        setSideBar(true)
+    }
+    return (
+        <>
+            <div className="navbar">
+                <div className="nav-left">
+
+                    <a href='/'><img className='logo' src={logo} alt="" /></a>
+                    <a href="/">
+                        <h1>TrackMate</h1>
+                    </a>
+                </div>
+                <div className="nav-right">
+                    <NavLink to="/"><i className="bi bi-person-circle"></i></NavLink>
+                    <NavLink to="/"><h3 className='login'>Log In</h3></NavLink>
+                    <i className="bi bi-bag-fill" onClick={handleSideBar}></i>
+                    <NavLink to="/tracmateShop"><button className='buyNow'>Buy Now</button></NavLink>
+                </div>
             </div>
-            <div className="nav-right">
-                <NavLink to="/"><h3>FAQ</h3></NavLink>
-                <NavLink to="/"><i className="bi bi-person-circle"></i></NavLink>
-                <NavLink to="/"><h3 className='login'>Log In</h3></NavLink>
-                <NavLink to="/"><i className="bi bi-bag-fill"></i></NavLink>
-                <NavLink to="/tracmateShop"><button className='buyNow'>Buy Now</button></NavLink>
+            <div className={`sideBar ${sideBar ? '' : 'sideBar1'}`}>
+                <button className='close' onClick={closeSideBar}><i class="bi bi-x-lg"></i></button>
             </div>
-        </div>
+        </>
     );
 }
 export default Navbar
